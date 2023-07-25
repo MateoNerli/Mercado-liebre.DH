f65bab2595d8982fc612e047bc076eeeb171964c
MateoNerli <mnerli2003@gmail.com>
2023-07-25 20:02:16 -0300

    Frist Commit; Pagina de mercado liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const puerto = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use("/", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
+app.listen(puerto, () => {
+  console.log("Aplicación esuchando en puerto 3001");
+});
